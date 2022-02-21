9342728d4d996e0aed6cbb5ae9e64283cb1eb9e8
Eugene <72197489+eugene-nn@users.noreply.github.com>
2022-02-21 17:42:14 +0100

    Change signaure function

--- zoom-session.js
+++ zoom-session.js
@@ -1,20 +1,38 @@
 const KJUR = require("jsrsasign");
 
-function generateVideoToken(sdkKey, sdkSecret, topic, password = "") {
+function generateVideoToken(
+  sdkKey,
+  sdkSecret,
+  topic,
+  passWord = "",
+  userIdentity = "",
+  sessionKey = ""
+) {
   let signature = "";
-  const iat = Math.round(new Date().getTime() / 1000);
-  const exp = iat + 60 * 60 * 2;
-  const oHeader = { alg: "HS256", typ: "JWT" };
-  const oPayload = {
-    app_key: sdkKey,
-    iat,
-    exp,
-    tpc: topic,
-    pwd: password,
-  };
-  const sHeader = JSON.stringify(oHeader);
-  const sPayload = JSON.stringify(oPayload);
-  signature = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, sdkSecret);
+  try {
+    const iat = Math.round(new Date().getTime() / 1000);
+    const exp = iat + 60 * 60 * 2;
+
+    // Header
+    const oHeader = { alg: "HS256", typ: "JWT" };
+    // Payload
+    const oPayload = {
+      app_key: sdkKey,
+      iat,
+      exp,
+      tpc: topic,
+      pwd: passWord,
+      user_identity: userIdentity,
+      session_key: sessionKey,
+      // topic
+    };
+    // Sign JWT, password=616161
+    const sHeader = JSON.stringify(oHeader);
+    const sPayload = JSON.stringify(oPayload);
+    signature = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, sdkSecret);
+  } catch (e) {
+    console.error(e);
+  }
   return signature;
 }
 
